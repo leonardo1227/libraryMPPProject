ce5921a04619ee815a88153eb6c4f77dd8003d49
108512 <108512@cslab-v32-404.cs.mum.edu>
2018-06-07 16:30:26 -0500

    Good progress on login

--- CS401MPPLibraryProject/src/edu/mum/cs401mpp/libraryproject/main/Main.java
+++ CS401MPPLibraryProject/src/edu/mum/cs401mpp/libraryproject/main/Main.java
@@ -2,6 +2,7 @@ package edu.mum.cs401mpp.libraryproject.main;
 
 import java.util.Scanner;
 
+import edu.mum.cs401mpp.libraryproject.entity.Stuff;
 import edu.mum.cs401mpp.libraryproject.service.LoginService;
 
 public class Main {
@@ -15,8 +16,48 @@ public class Main {
 		String uname = scan.next();
 		System.out.println("Enter password");
 		String pword = scan.next();
-		
-		LoginService.checkUsernameandPassword(uname, pword);
+		Stuff s = LoginService.checkUsernameandPassword(uname, pword);
+		if(s != null){
+			String authlevel = s.getAuthorizationLevel().getDescription();
+			
+			if(authlevel.equalsIgnoreCase("Librarian")) {
+				//LibrarianService.checkoutBook();
+			}else if(authlevel.equalsIgnoreCase("Admin")){
+				boolean looper = true;
+				while(looper){
+				System.out.println("Type 1 to add new member or 2 to add new book or 0 to stop");
+				int i = scan.nextInt();
+				
+				if(i == 1){
+					//AdminService.addnewMember();
+				}else if(i==2){
+					//AdminService.addnewCopy();
+				}else if(i==0)
+					looper = false;
+				else
+					System.out.println("Invalid input");
+				}
+			}else if(authlevel.equalsIgnoreCase("Both")){
+				boolean looper = true;
+				while(looper){
+				System.out.println("Type 1 to add new member, 2 to add new book, 3 add member, or 0 to stop");
+				int i = scan.nextInt();
+				
+				if(i == 1){
+					//AdminService.addnewMember();
+				}else if(i==2){
+					//AdminService.addnewCopy();
+				}else if(i==3){
+					//LibraryService.checkoutBook();
+				}else if(i==0)
+					looper = false;
+				else
+					System.out.println("Invalid input");
+				}
+				
+			}
+			
+		}
 		
 		scan.close();
 		
--- CS401MPPLibraryProject/src/edu/mum/cs401mpp/libraryproject/service/LoginService.java
+++ CS401MPPLibraryProject/src/edu/mum/cs401mpp/libraryproject/service/LoginService.java
@@ -4,17 +4,8 @@ import edu.mum.cs401mpp.libraryproject.entity.Stuff;
 
 public class LoginService {
 
-	public static void checkUsernameandPassword(String uname, String pword) {
-		Stuff temp = AuthenticationService.checkAuthentication(uname, pword);
-		
-		//checking role type
-		if(temp.getAuthorizationLevel().getDescription().equalsIgnoreCase("Librarian"))
-			//checkoutbook option
-		else if
-			
-		
-		
-		
+	public static Stuff checkUsernameandPassword(String uname, String pword) {
+		return  AuthenticationService.checkAuthentication(uname, pword);
 	}
 	
 }
