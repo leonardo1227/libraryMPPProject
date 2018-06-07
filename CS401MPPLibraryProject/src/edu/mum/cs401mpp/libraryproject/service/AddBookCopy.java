cee13ae50c55753162aad2c7369e4d1c908cab05
batbileg <batbileg@10.10.51.83>
2018-06-07 15:21:34 -0500

    just created empty class

---
+++ CS401MPPLibraryProject/src/edu/mum/cs401mpp/libraryproject/service/AddBookCopy.java
@@ -0,0 +1,17 @@
+package edu.mum.cs401mpp.libraryproject.service;
+
+public class AddBookCopy {
+	
+	public void addBookCopy(int ISBN) {
+		//lookup book by ISBN
+		
+		//create new book copy
+		
+		//add new book copy to book collection and persist
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
